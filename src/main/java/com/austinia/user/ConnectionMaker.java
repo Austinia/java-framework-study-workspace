8b6999b873faba50193fe1d297b6134ae7652127
Austinia <kimdy90919@gmail.com>
2023-01-06 17:06:11 +0900

    ConnectionMaker 수정
    주석 제거

--- src/main/java/com/austinia/user/ConnectionMaker.java
+++ src/main/java/com/austinia/user/ConnectionMaker.java
@@ -7,10 +7,4 @@ import java.sql.SQLException;
 public interface ConnectionMaker {
 
     public Connection getConnection() throws ClassNotFoundException, SQLException;
-//        // driver
-//        Class.forName("com.mysql.cj.jdbc.Driver");
-//        // connection
-//        Connection connection = DriverManager.getConnection("jdbc:mysql://localhost/jejunu", "jeju", "jeju");
-//        return connection;
-
 }
