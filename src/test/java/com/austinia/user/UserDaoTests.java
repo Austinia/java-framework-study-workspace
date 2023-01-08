eb8211d2af33b3396e322d4ffd1f3c86a0c521fe
Austinia <kimdy90919@gmail.com>
2023-01-08 15:55:59 +0900

    Blog 23
    GenericGroovyApplicationContext

--- src/test/java/com/austinia/user/UserDaoTests.java
+++ src/test/java/com/austinia/user/UserDaoTests.java
@@ -4,7 +4,7 @@ import org.hamcrest.core.IsNull;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.context.support.GenericGroovyApplicationContext;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.greaterThan;
@@ -16,7 +16,7 @@ public class UserDaoTests {
 
     @BeforeAll
     public static void setup() {
-        ApplicationContext applicationContext = new ClassPathXmlApplicationContext("daoFactory.xml");
+        ApplicationContext applicationContext = new GenericGroovyApplicationContext("daoFactory.groovy");
         userDao = applicationContext.getBean("userDao", UserDao.class);
     }
 
