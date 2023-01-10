6619da800a0f42ff4301b267fa1b196e37c084cb
Austinia <kimdy90919@gmail.com>
2023-01-10 14:21:29 +0900

    Blog 26
    Lombok

--- src/main/java/com/austinia/user/User.java
+++ src/main/java/com/austinia/user/User.java
@@ -1,31 +1,13 @@
 package com.austinia.user;
 
+import lombok.*;
+
+@Data
+@Builder // 빌더 패턴을 사용한다
+@NoArgsConstructor // 기본 생성자를 만들어준다
+@AllArgsConstructor // 빌더는 모든 전달인자를 생성자를 모두 포함한 생성자가 필요하다
 public class User {
     private Integer id;
     private String name;
     private String password;
-
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
 }
---
+++ src/test/java/com/austinia/user/LombokTests.java
@@ -0,0 +1,18 @@
+package com.austinia.user;
+
+import org.junit.jupiter.api.Test;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.Is.is;
+
+public class LombokTests {
+    @Test
+    public void equals(){
+        // builder 패턴은 value들을 메서드를 통해서 정의할 수 있게 한다
+        User user = User.builder().id(1).name("Updated Hulk").password("1234").build();
+        User user2 = User.builder().id(1).name("Updated Hulk").password("1234").build();
+
+        assertThat(user, is(user2));
+
+    }
+}
