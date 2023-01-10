20fa9d7d6a4b20eb5d89c7adb4c56b5bd5fe8c2d
Austinia <kimdy90919@gmail.com>
2023-01-10 16:56:18 +0900

    Blog 27
    Java Web Application - Servlet w/ Tomcat

---
+++ src/main/java/com/austinia/user/UserServlet.java
@@ -0,0 +1,39 @@
+package com.austinia.user;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import javax.servlet.GenericServlet;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import java.io.IOException;
+
+public class UserServlet extends GenericServlet {
+    private UserDao userDao;
+    @Override
+    public void init() throws ServletException {
+        System.out.println("**************** init ****************");
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext("com.austinia.user");
+        this.userDao = applicationContext.getBean("userDao", UserDao.class);
+        super.init();
+    }
+
+    @Override
+    public void service(ServletRequest req, ServletResponse res) throws ServletException, IOException {
+        User user = userDao.get(1);
+        StringBuffer stringBuffer = new StringBuffer();
+        stringBuffer.append("<html>");
+        stringBuffer.append("<h1>");
+        stringBuffer.append(String.format("Hello %s!!", user.getName()));
+        stringBuffer.append("</h1>");
+        stringBuffer.append("</html>");
+        res.getWriter().println(stringBuffer);
+    }
+
+    @Override
+    public void destroy() {
+        System.out.println("**************** destroy ****************");
+        super.destroy();
+    }
+}
