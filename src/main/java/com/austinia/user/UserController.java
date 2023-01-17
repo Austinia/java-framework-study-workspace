3ed63054171b805ae585dd8517406ad045a0b5c4
Austinia <kimdy90919@gmail.com>
2023-01-17 15:16:17 +0900

    Blog 36
    RequestMapping, Method Param & Method Return

--- src/main/java/com/austinia/user/UserController.java
+++ src/main/java/com/austinia/user/UserController.java
@@ -2,10 +2,7 @@ package com.austinia.user;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -13,24 +10,26 @@ import javax.servlet.http.HttpServletRequest;
 import java.io.*;
 
 @Controller
-@RequestMapping // RequestMethod"HandlerMapping"에 의해서 찾아진다
+@RequestMapping // RequestMethod"HandlerMapping"에 의해서 찾아진다, 생략 가능
 @RequiredArgsConstructor
 public class UserController {
     private final UserDao userDao;
 
-    @RequestMapping("/user") // RequestMapping"HandlerAdapter"에 의해서 찾아진다
+    @RequestMapping(path = "/user") // RequestMapping"HandlerAdapter"에 의해서 찾아진다
     public User getUser(@RequestParam("id") Integer id) {
         return userDao.get(id);
     }
 
     // GET 메서드로 화면을 출력
-    @RequestMapping(path = "/upload", method = RequestMethod.GET)
-    public void upload(){
+//    @RequestMapping(path = "/upload", method = RequestMethod.GET)
+    @GetMapping("/upload")
+    public void upload() {
 
     }
 
-    @RequestMapping(path = "/upload", method = RequestMethod.POST)
-    public ModelAndView upload(@RequestParam("file")MultipartFile file, HttpServletRequest request) throws IOException {
+    //    @RequestMapping(path = "/upload", method = RequestMethod.POST)
+    @PostMapping("/upload")
+    public ModelAndView upload(@RequestParam("file") MultipartFile file, HttpServletRequest request) throws IOException {
         // 파일을 정의한다 ( 저장되는 장소를 지정 ), 서버의 물리적인 PATH를 받아서 파일의 오리지널 이름으로 저장한다
         File path = new File(request.getSession().getServletContext().getRealPath("/") + "/WEB-INF/static/" + file.getOriginalFilename());
         // 업로드된 파일의 바이트스트림을 저장한다
