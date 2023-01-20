0e384ce4d8cb0ea03d95350d633d9ddabbe86cf1
Austinia <kimdy90919@gmail.com>
2023-01-20 17:24:23 +0900

    USER CRUD
    GET, POST, PUT, DELETE

--- src/main/java/com/austinia/user/User.java
+++ src/main/java/com/austinia/user/User.java
@@ -14,5 +14,5 @@ public class User {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
     private String name;
-    private  String password;
+    private String password;
 }
--- src/main/java/com/austinia/user/UserController.java
+++ src/main/java/com/austinia/user/UserController.java
@@ -1,10 +1,7 @@
 package com.austinia.user;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
@@ -13,7 +10,26 @@ public class UserController {
     private final UserDao userDao;
 
     @GetMapping("/{id}")
-    public User get(@PathVariable("id") Integer id){
+    public User get(@PathVariable("id") Integer id) {
         return userDao.findById(id).get();
+    }
+
+    @PostMapping("/upload")
+    public User upload(User user) {
+        userDao.save(user);
+        return user;
+    }
+
+    @PutMapping("/update")
+    public void update(@RequestBody User user) {
+        User tmpuser = userDao.findById(user.getId()).get();
+        tmpuser.setName(user.getName());
+        tmpuser.setPassword(user.getPassword());
+        userDao.save(tmpuser);
+    }
+
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable("id") Integer id) {
+        userDao.deleteById(id);
     }
 }
