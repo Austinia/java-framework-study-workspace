53f527f45ede3a05d4455b89c4bd05029d5ade65
Austinia <kimdy90919@gmail.com>
2023-02-01 14:14:42 +0900

    2023/02/01 AOP
    쿼리 성능 체크 로깅 AOP 추가
    실행되는 메서드 앞 뒤로 메세지와 쿼리실행 시간을 알려줌

---
+++ src/main/java/com/austinia/user/UserLogAspect.java
@@ -0,0 +1,38 @@
+package com.austinia.user;
+
+import lombok.SneakyThrows;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+import org.springframework.util.StopWatch;
+
+@Aspect
+@Component
+public class UserLogAspect {
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
+
+    // PointCut : 적용할 지점 또는 범위
+    @Pointcut("execution(public * com.austinia.user.UserDao.*(..))")
+    private void publicTarget() {}
+
+    // Around : 실제 부가기능 구현
+    @SneakyThrows
+    @Around("publicTarget()")
+    public Object queryPerformanceAdvice(ProceedingJoinPoint proceedingJoinPoint) {
+        logger.info("쿼리 성능 측정을 시작합니다");
+        StopWatch stopWatch = new StopWatch();
+        stopWatch.start();
+
+        // Service Method
+        Object result = proceedingJoinPoint.proceed();
+
+        stopWatch.stop();
+        logger.info("쿼리 성능 측정이 끝났습니다");
+        logger.info("쿼리 실행 시간: {} ms", stopWatch.getLastTaskTimeMillis());
+        return result;
+    }
+}
