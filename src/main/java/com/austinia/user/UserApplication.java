8ef189e3f7734afe44e14712b42d4509fad7a078
Austinia <kimdy90919@gmail.com>
2023-02-03 03:11:32 +0900

    2023/02/03 Security
    CORS: /api/**의 path로 http://localhost:3000을 허락된 Origin으로 설정

--- src/main/java/com/austinia/user/UserApplication.java
+++ src/main/java/com/austinia/user/UserApplication.java
@@ -5,6 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class UserApplication {
@@ -13,6 +15,17 @@ public class UserApplication {
     public PasswordEncoder passwordEncoder() {
         return PasswordEncoderFactories.createDelegatingPasswordEncoder();
     }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/api/**").allowedOrigins("http://localhost:3000");
+            }
+        };
+    }
+
     public static void main(String[] args) {
         SpringApplication.run(UserApplication.class, args);
     }
--- src/main/java/com/austinia/user/UserSecurityConfig.java
+++ src/main/java/com/austinia/user/UserSecurityConfig.java
@@ -2,6 +2,7 @@ package com.austinia.user;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
@@ -12,10 +13,9 @@ public class UserSecurityConfig {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
-        httpSecurity.csrf().disable().authorizeHttpRequests().antMatchers("/api/user").permitAll();
-//                .antMatchers(HttpMethod.GET, "/api/**").permitAll()
-//                .antMatchers("/auth/**").permitAll()
-//                .anyRequest().authenticated();
+        httpSecurity.csrf().disable().authorizeHttpRequests()
+                .antMatchers(HttpMethod.GET, "/api/**").permitAll()
+                .anyRequest().permitAll();
         return httpSecurity.build();
     }
 }
